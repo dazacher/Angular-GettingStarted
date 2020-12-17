566b2db8c6d4bba14e44d416bb7154f6d63a1442
dazacher <dazacher2000@yahoo.com>
2020-12-17 09:41:36 -0700

    Added product-list components.

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   selector: 'pm-root' ,
   template: `
     <div><h1>{{pageTitle}}</h1>
-      <div>My First Component</div>
-</div>`
+      <pm-products></pm-products>
+  </div>`
 })
 
 export class AppComponent {
